48bf2a322424900a5128379cae51b0da8c4d0701
fourside <fourside@gmail.com>
2018-11-03 19:58:15 +0900

    example keycode

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import Slide from './slide.js';
 
+import keycode from 'keycode';
+
+document.onkeyup = event => {
+  if (['right', 'j'].some(key => keycode.isEventKey(event, key))) {
+    console.log('next');
+  }
+  if (['left', 'k'].some(key => keycode.isEventKey(event, key))) {
+    console.log('prev');
+  }
+}
+
