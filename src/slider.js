404225d2b9212379a70e5fb191551704db68c149
fourside <fourside@gmail.com>
2018-11-03 21:01:26 +0900

    toggle active slide by key event

--- src/slider.js
+++ src/slider.js
@@ -1,12 +1,16 @@
 import {render, html} from 'lit-html';
 import keycode from 'keycode';
 
+const ACTIVE_CLASS = "active";
+
 export default class Slider extends HTMLElement {
+
   constructor() {
     super();
     this.attachShadow({mode: 'open'});
-    addListner(document);
     render(this.template, this.shadowRoot);
+    this.addListner(document);
+    this.activate(0);
   }
 
   get template() {
@@ -14,17 +18,53 @@ export default class Slider extends HTMLElement {
       <slot></slot>
     `;
   }
-}
 
-function addListner(element) {
-  element.onkeyup = event => {
-    if (['right', 'j'].some(key => keycode.isEventKey(event, key))) {
-      console.log('next');
-    }
-    if (['left', 'k'].some(key => keycode.isEventKey(event, key))) {
-      console.log('prev');
+  max() {
+    return this.children.length -1;
+  }
+
+  addListner(element) {
+    element.onkeyup = event => {
+      if (['right', 'j'].some(key => keycode.isEventKey(event, key))) {
+        this.next();
+      }
+      if (['left', 'k'].some(key => keycode.isEventKey(event, key))) {
+        this.prev();
+      }
     }
   }
+
+  activate(index) {
+    Array.prototype.forEach.call(this.children, (child, i) => {
+      if (i === index) {
+        child.classList.add(ACTIVE_CLASS);
+      } else {
+        child.classList.remove(ACTIVE_CLASS);
+      }
+    });
+  }
+
+  current() {
+    const index = Array.prototype.findIndex.call(this.children, (child) => {
+      return child.classList.contains(ACTIVE_CLASS);
+    });
+    return index > -1 ? index : 0;
+  }
+
+  next() {
+    const i = this.current();
+    if (i < this.max()) {
+      this.activate(i + 1);
+    }
+  }
+
+  prev() {
+    const i = this.current();
+    if (i > 0) {
+      this.activate(i - 1);
+    }
+  }
+
 }
 
 customElements.define('x-slider', Slider);
