9ae921096d19fd9196c972df88f1d4ce39c950d9
fourside <fourside@gmail.com>
2018-11-04 16:31:57 +0900

    use css loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  module: {
+    rules: [
+      { test: /\.css/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            }
+          }
+        ]}
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({template: './src/index.html'}),
     new CopyWebpackPlugin([
